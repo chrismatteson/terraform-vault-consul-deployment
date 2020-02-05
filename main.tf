f2ba6a7b5c8f5e3735fb1fc3283f8285e1352a53
Yonathan Koren <yonkornilov@live.com>
2020-02-05 09:58:57 -0600

    Add root-level Terraform configuration to call modules.aws five times (for each Vault cluster).

---
+++ main.tf
@@ -0,0 +1,49 @@
+module "primary_cluster" {
+  source                     = "./modules/aws"
+  region                     = "us-east-1"
+  consul_cluster_size        = 6
+  vault_cluster_size         = 3
+  consul_ent_license         = var.consul_ent_license
+  vault_ent_license          = var.vault_ent_license
+  enable_deletion_protection = false
+}
+
+module "dr_cluster" {
+  source                     = "./modules/aws"
+  region                     = "us-west-2"
+  consul_cluster_size        = 1
+  vault_cluster_size         = 1
+  consul_ent_license         = var.consul_ent_license
+  vault_ent_license          = var.vault_ent_license
+  enable_deletion_protection = false
+}
+
+module "eu_cluster" {
+  source                     = "./modules/aws"
+  region                     = "eu-central-1"
+  consul_cluster_size        = 1
+  vault_cluster_size         = 1
+  consul_ent_license         = var.consul_ent_license
+  vault_ent_license          = var.vault_ent_license
+  enable_deletion_protection = false
+}
+
+module "eu_dr_cluster" {
+  source                     = "./modules/aws"
+  region                     = "eu-west-1"
+  consul_cluster_size        = 1
+  vault_cluster_size         = 1
+  consul_ent_license         = var.consul_ent_license
+  vault_ent_license          = var.vault_ent_license
+  enable_deletion_protection = false
+}
+
+module "ap_cluster" {
+  source                     = "./modules/aws"
+  region                     = "ap-southeast-1"
+  consul_cluster_size        = 1
+  vault_cluster_size         = 1
+  consul_ent_license         = var.consul_ent_license
+  vault_ent_license          = var.vault_ent_license
+  enable_deletion_protection = false
+}
---
+++ variables.tf
@@ -0,0 +1,3 @@
+# Supply the following two variables using a tfvars file
+variable "consul_ent_license" {}
+variable "vault_ent_license" {}
