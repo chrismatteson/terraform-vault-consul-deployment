b69e43fe92063649dd2f8936d1320d12b1622ca1
Yonathan Koren <yonkornilov@live.com>
2020-02-05 13:26:28 -0600

    Ensure enterprise binaries are used - so that enterprise features are present.

--- main.tf
+++ main.tf
@@ -2,7 +2,9 @@ module "primary_cluster" {
   source                     = "./modules/aws"
   region                     = "us-west-1"
   consul_cluster_size        = 6
+  consul_version             = var.consul_version
   vault_cluster_size         = 3
+  vault_version              = var.vault_version
   consul_ent_license         = var.consul_ent_license
   vault_ent_license          = var.vault_ent_license
   subnet_second_octet        = "0"
@@ -14,7 +16,9 @@ module "dr_cluster" {
   source                     = "./modules/aws"
   region                     = "us-west-2"
   consul_cluster_size        = 1
+  consul_version             = var.consul_version
   vault_cluster_size         = 1
+  vault_version              = var.vault_version
   consul_ent_license         = var.consul_ent_license
   vault_ent_license          = var.vault_ent_license
   subnet_second_octet        = "1"
@@ -26,7 +30,9 @@ module "eu_cluster" {
   source                     = "./modules/aws"
   region                     = "eu-central-1"
   consul_cluster_size        = 1
+  consul_version             = var.consul_version
   vault_cluster_size         = 1
+  vault_version              = var.vault_version
   consul_ent_license         = var.consul_ent_license
   vault_ent_license          = var.vault_ent_license
   subnet_second_octet        = "2"
@@ -38,7 +44,9 @@ module "eu_dr_cluster" {
   source                     = "./modules/aws"
   region                     = "eu-west-1"
   consul_cluster_size        = 1
+  consul_version             = var.consul_version
   vault_cluster_size         = 1
+  vault_version              = var.vault_version
   consul_ent_license         = var.consul_ent_license
   vault_ent_license          = var.vault_ent_license
   subnet_second_octet        = "4"
@@ -50,7 +58,9 @@ module "ap_cluster" {
   source                     = "./modules/aws"
   region                     = "ap-southeast-1"
   consul_cluster_size        = 1
+  consul_version             = var.consul_version
   vault_cluster_size         = 1
+  vault_version              = var.vault_version
   consul_ent_license         = var.consul_ent_license
   vault_ent_license          = var.vault_ent_license
   subnet_second_octet        = "4"
--- variables.tf
+++ variables.tf
@@ -6,3 +6,13 @@ variable "force_bucket_destroy" {
   default     = true
   description = "Whether or not to force destroy s3 buckets that are non-empty."
 }
+
+variable "consul_version" {
+  default = "1.6.3+ent"
+  description = "Which version of the consul binary to download from releases.hashicorp.com."
+}
+
+variable "vault_version" {
+  default = "1.3.2+ent"
+  description = "Which version of the vault binary to download from releases.hashicorp.com."
+}
