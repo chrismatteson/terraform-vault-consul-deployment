3cb059b57459d03fd7bc6fa1329fe93efa24813c
Yonathan Koren <yonkornilov@live.com>
2020-02-06 10:47:22 -0600

    Use heredoc for policy file, use hashicorp-recommended policy for auth ec2 method.

--- use-cases/auth-methods/ec2/script-vault.sh
+++ use-cases/auth-methods/ec2/script-vault.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 vault auth enable aws
-vault write auth/aws/config/client secret_key=$AWS_ACCESS_KEY_ID  access_key=$AWS_SECRET_ACCESS_KEY
-vault write auth/aws/role/app-role auth_type=ec2 bound_ec2_instance_id=$APP_INSTANCE_ID policies=getmysqlcreds
+vault write auth/aws/config/client access_key=$AWS_ACCESS_KEY_ID secret_key=$AWS_SECRET_ACCESS_KEY
+vault write auth/aws/role/app-role auth_type=ec2 allow_instance_migration=true bound_ec2_instance_id=$APP_INSTANCE_ID policies=getmysqlcreds
--- use-cases/auth-methods/ec2/script.sh
+++ use-cases/auth-methods/ec2/script.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-aws iam create-policy --policy-name vault-ec2-auth-validate --policy-document file://policy.json
+cat << EOF > /tmp/vault-ec2-auth-validate-policy.json
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ec2:DescribeInstances",
+        "iam:GetInstanceProfile",
+        "iam:GetUser",
+        "iam:GetRole"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": ["sts:AssumeRole"],
+      "Resource": ["arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/vault-ec2-auth-validate"]
+    }
+  ]
+}
+EOF
+
+aws iam create-policy --policy-name vault-ec2-auth-validate --policy-document file:///tmp/vault-ec2-auth-validate-policy.json
 aws iam create-user --user-name vault-ec2-auth-validate
 aws iam attach-user-policy --policy-arn $(aws iam list-policies --query 'Policies[?PolicyName==`vault-ec2-auth-validate`].Arn' --output text) --user-name vault-ec2-auth-validate
 aws iam create-access-key --user-name vault-ec2-auth-validate
